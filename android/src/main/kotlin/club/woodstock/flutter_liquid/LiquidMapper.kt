8a6ad89b9f80bd112238090326151df9d193c733
Takashi Kasai <takassh23@gmail.com>
2024-09-24 17:47:51 +0900

    Implement HE method integration

--- android/src/main/kotlin/club/woodstock/flutter_liquid/LiquidMapper.kt
+++ android/src/main/kotlin/club/woodstock/flutter_liquid/LiquidMapper.kt
@@ -1,10 +1,21 @@
 package club.woodstock.flutter_liquid
 
+import android.R.attr.bitmap
 import asia.liquidinc.ekyc.applicant.external.result.LiquidChipIdentificationResult
+import asia.liquidinc.ekyc.applicant.external.result.LiquidChipVerificationResult
+import asia.liquidinc.ekyc.applicant.external.result.LiquidFaceVerificationResult
 import asia.liquidinc.ekyc.applicant.external.result.LiquidProcessingResult
+import asia.liquidinc.ekyc.applicant.external.result.auto.AutoVerificationFace
+import asia.liquidinc.ekyc.applicant.external.result.auto.AutoVerificationFacePassive
+import asia.liquidinc.ekyc.applicant.external.result.auto.AutoVerificationFacePhoto
+import asia.liquidinc.ekyc.applicant.external.result.auto.AutoVerificationResult
 import asia.liquidinc.ekyc.applicant.external.result.chip.LiquidChipData
+import asia.liquidinc.ekyc.applicant.external.result.document.DocumentImage
+import asia.liquidinc.ekyc.applicant.external.result.face.FaceImage
 import asia.liquidinc.ekyc.applicant.external.result.jpki.JpkiEvidence
 import asia.liquidinc.ekyc.applicant.external.result.jpki.JpkiResult
+import java.nio.ByteBuffer
+
 
 fun JpkiResult.toMap(): Map<String, Any?> {
     return mapOf(
@@ -24,6 +35,11 @@ fun JpkiEvidence.toMap(): Map<String, Any?> {
 }
 
 fun LiquidChipData.toMap(): Map<String, Any?> {
+    var idFacePhotoByteBuffer: ByteBuffer? = null
+    if (idFacePhoto != null) {
+        idFacePhotoByteBuffer = ByteBuffer.allocate(idFacePhoto.byteCount)
+        idFacePhoto.copyPixelsToBuffer(idFacePhotoByteBuffer)
+    }
     return mapOf(
         "name" to name,
         "nameKana" to nameKana,
@@ -44,10 +60,34 @@ fun LiquidChipData.toMap(): Map<String, Any?> {
         "residenceCardUpdateStatus" to isResidenceCardUpdateStatus,
         "residenceCardInfoType" to liquidResidenceCardInfoType?.name,
         "residenceCardType" to liquidResidenceCardType?.name,
-        "idFacePhoto" to idFacePhoto,
-        "nameExternalCharacters" to nameExternalCharacters,
-        "previousNameExternalCharacters" to previousNameExternalCharacters,
-        "addressExternalCharacters" to addressExternalCharacters,
+        "idFacePhoto" to idFacePhotoByteBuffer?.array(),
+        "nameExternalCharacters" to nameExternalCharacters?.mapNotNull {
+            if (it == null) {
+                null
+            } else {
+                val buffer = ByteBuffer.allocate(it.byteCount)
+                it.copyPixelsToBuffer(buffer)
+                buffer.array()
+            }
+        },
+        "previousNameExternalCharacters" to previousNameExternalCharacters?.mapNotNull {
+            if (it == null) {
+                null
+            } else {
+                val buffer = ByteBuffer.allocate(it.byteCount)
+                it.copyPixelsToBuffer(buffer)
+                buffer.array()
+            }
+        },
+        "addressExternalCharacters" to addressExternalCharacters?.mapNotNull {
+            if (it == null) {
+                null
+            } else {
+                val buffer = ByteBuffer.allocate(it.byteCount)
+                it.copyPixelsToBuffer(buffer)
+                buffer.array()
+            }
+        },
         "isExistLatestName" to isExistLatestName,
         "isExistLatestAddress" to isExistLatestAddress,
     )
@@ -77,5 +117,97 @@ fun LiquidProcessingResult.toMap(): Map<String, Any?> {
             "maintenanceTitle" to additionalDataTitle,
             "maintenanceMessage" to additionalDataMessage,
         ),
+    )
+}
+
+fun LiquidChipVerificationResult.toMap(): Map<String, Any?> {
+    return mapOf(
+        "result" to mapOf(
+            "status" to resultStatus.name,
+            "resultCode" to errorCode,
+            "additionalData" to mapOf(
+                "maintenanceTitle" to additionalDataTitle,
+                "maintenanceMessage" to additionalDataMessage,
+            ),
+        ),
+        "autoVerificationFacePhoto" to autoVerificationResult.autoVerificationFacePhoto?.toMap(),
+        "autoVerificationFace" to autoVerificationResult.autoVerificationFace?.toMap(),
+        "autoVerificationFacePassive" to autoVerificationResult.autoVerificationFacePassive?.toMap(),
+        "documentImage" to documentImage?.toMap(),
+        "chipData" to liquidChipData?.toMap(),
+    )
+}
+
+fun AutoVerificationFacePhoto.toMap(): Map<String, Any?> {
+    return mapOf(
+        "resultType" to resultType.ordinal,
+        "score" to score,
+    )
+}
+
+fun AutoVerificationFace.toMap(): Map<String, Any?> {
+    return mapOf(
+        "resultType" to resultType.ordinal,
+        "score" to score,
+    )
+}
+
+fun AutoVerificationFacePassive.toMap(): Map<String, Any?> {
+    return mapOf(
+        "result" to result,
+    )
+}
+
+fun DocumentImage.toMap(): Map<String, Any?> {
+    var frontByteBuffer: ByteBuffer? = null
+    if (front != null) {
+        frontByteBuffer = ByteBuffer.allocate(front.byteCount)
+        front.copyPixelsToBuffer(frontByteBuffer)
+    }
+    var diagonalByteBuffer: ByteBuffer? = null
+    if (diagonal != null) {
+        diagonalByteBuffer = ByteBuffer.allocate(diagonal.byteCount)
+        diagonal.copyPixelsToBuffer(diagonalByteBuffer)
+    }
+    var backByteBuffer: ByteBuffer? = null
+    if (back != null) {
+        backByteBuffer = ByteBuffer.allocate(back.byteCount)
+        back.copyPixelsToBuffer(backByteBuffer)
+    }
+    return mapOf(
+        "front" to frontByteBuffer?.array(),
+        "diagonal" to diagonalByteBuffer?.array(),
+        "back" to backByteBuffer?.array(),
+    )
+}
+
+fun LiquidFaceVerificationResult.toMap(): Map<String, Any?> {
+    return mapOf(
+        "result" to mapOf(
+            "status" to resultStatus.name,
+            "resultCode" to errorCode,
+            "additionalData" to mapOf(
+                "maintenanceTitle" to additionalDataTitle,
+                "maintenanceMessage" to additionalDataMessage,
+            ),
+        ),
+        "autoVerificationFacePhoto" to autoVerificationResult.autoVerificationFacePhoto?.toMap(),
+        "autoVerificationFace" to autoVerificationResult.autoVerificationFace?.toMap(),
+        "autoVerificationFacePassive" to autoVerificationResult.autoVerificationFacePassive?.toMap(),
+        "faceImage" to faceImage?.toMap(),
+    )
+}
+
+fun FaceImage.toMap(): Map<String, Any?> {
+    val faceFrontByteBuffer = ByteBuffer.allocate(faceFront.byteCount)
+    faceFront.copyPixelsToBuffer(faceFrontByteBuffer)
+
+    return mapOf(
+        "faceFront" to faceFrontByteBuffer.array(),
+        "liveness" to liveness.map {
+            val buffer = ByteBuffer.allocate(it.byteCount)
+            it.copyPixelsToBuffer(buffer)
+            buffer.array()
+        },
     )
 }
--- ios/Classes/FlutterLiquidPlugin.swift
+++ ios/Classes/FlutterLiquidPlugin.swift
@@ -3,95 +3,283 @@ import Liquid
 import UIKit
 
 public class FlutterLiquidPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(
-      name: "flutter_liquid",
-      binaryMessenger: registrar.messenger()
-    )
-    let instance = FlutterLiquidPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getVersion":
-      getVersion(call, result: result)
-    case "startVerify":
-      startVerify(call, result: result)
-    case "identifyIdChip":
-      identifyIdChip(call, result: result)
-    case "activate":
-      activate(call, result: result)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(
+        with registrar: FlutterPluginRegistrar
+    ) {
+        let channel = FlutterMethodChannel(
+            name: "flutter_liquid",
+            binaryMessenger: registrar.messenger()
+        )
+        let instance = FlutterLiquidPlugin()
+        registrar.addMethodCallDelegate(
+            instance,
+            channel: channel
+        )
     }
-  }
-  
-  func getVersion(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result(LiquidEkyc.getVersion())
-  }
-
-  func startVerify(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    guard let args = call.arguments as? [String: Any?] else {
-      return
+    
+    public func handle(
+        _ call: FlutterMethodCall,
+        result: @escaping FlutterResult
+    ) {
+        switch call.method {
+        case "getVersion":
+            getVersion(
+                call,
+                result: result
+            )
+        case "startVerify":
+            startVerify(
+                call,
+                result: result
+            )
+        case "identifyIdChip":
+            identifyIdChip(
+                call,
+                result: result
+            )
+        case "verifyIdChip":
+            verifyIdChip(
+                call,
+                result: result
+            )
+        case "verifyFace":
+            verifyFace(
+                call,
+                result: result
+            )
+        case "activate":
+            activate(
+                call,
+                result: result
+            )
+        default:
+            result(
+                FlutterMethodNotImplemented
+            )
+        }
     }
-    guard let endpoint = args["endpoint"] as? String else {
-      return
+    
+    func getVersion(
+        _ call: FlutterMethodCall,
+        result: @escaping FlutterResult
+    ) {
+        result(
+            LiquidEkyc.getVersion()
+        )
     }
-    guard let apiKey = args["apiKey"] as? String else {
-      return
+    
+    
+    
+    func startVerify(
+        _ call: FlutterMethodCall,
+        result: @escaping FlutterResult
+    ) {
+        guard let args = call.arguments as? [String: Any?] else {
+            return
+        }
+        guard let endpoint = args["endpoint"] as? String else {
+            return
+        }
+        guard let apiKey = args["apiKey"] as? String else {
+            return
+        }
+        guard let applicant = args["applicant"] as? String? else {
+            return
+        }
+        guard let token = args["token"] as? String? else {
+            return
+        }
+        
+        if applicant != nil && token != nil {
+            LiquidEkyc.startVerify(endpoint: URL(
+                fileURLWithPath: endpoint
+            ), applicant: applicant!, token: token!)
+            
+            result(
+                nil
+            )
+            return
+        }
+        
+        let _ = LiquidEkyc.startVerify(
+            endpoint: URL(
+                fileURLWithPath: endpoint
+            ),
+            apiKey: apiKey,
+            completion: {
+                param in
+                result(
+                    param.toDictionary()
+                )
+            }
+        )
     }
-    guard let applicant = args["applicant"] as? String? else {
-      return
+    
+    func identifyIdChip(
+        _ call: FlutterMethodCall,
+        result: @escaping FlutterResult
+    ) {
+        guard
+            let rootViewController = UIApplication.shared.windows.filter({
+                (
+                    w
+                ) -> Bool in
+                return w.isHidden == false
+            }).first?.rootViewController
+        else {
+            result(
+                FlutterError()
+            )
+            return
+        }
+        
+        let parameters = IdentifyIdChipParametersBuilder()
+            .setVerificationMethodJpki(
+                .complyWa
+            )
+            .setEnabledChipForgotPin(
+                false
+            )
+            .setDocumentTypeJpki(
+                .mynumberCardWithMyNumber
+            )
+            .build()
+        LiquidEkyc.identifyIdChip(parameters,
+                                  on: rootViewController,
+                                  completion: {
+            chipResult in
+            result(
+                chipResult.toDictionary()
+            )
+        })
     }
-    guard let token = args["token"] as? String? else {
-      return
+    
+    func verifyIdChip(
+        _ call: FlutterMethodCall,
+        result: @escaping FlutterResult
+    ) {
+        guard let args = call.arguments as? [String: Any?] else {
+            result(FlutterError(
+                code: "argument",
+                message: "liquid_document_type and verification_method are required",
+                details:"liquid_document_type and verification_method are required"
+                )
+            )
+            return
+        }
+        guard let liquidDocumentTypeStr = args["liquid_document_type"] as? String else {
+            result(FlutterError(
+                code: "argument",
+                message: "liquid_document_type is required",
+                details:"liquid_document_type is required"
+                )
+            )
+            return
+        }
+        guard let verificationMethodStr = args["verification_method"] as? String else {
+            result(FlutterError(
+                code:"argument",
+                message:"verification_method is required",
+                details:"verification_method is required"
+                )
+            )
+            return
+        }
+        guard let liquidDocumentType = IdDocumentType.fromString(liquidDocumentTypeStr) else {
+            var values = [String]()
+                    for type in IdDocumentType.allCases {
+                        values.append(type.toString())
+                    }
+            result(
+                FlutterError(code: "argument",
+                             message:"liquid_document_type must be chosen from \(values)",
+                             details:"liquid_document_type must be chosen from \(values)")
+            )
+            return
+        }
+        guard let verificationMethod = VerificationMethod.fromString(verificationMethodStr) else {
+            result(
+                FlutterError(code: "argument",
+                             message:"verification_method must be chosen from COMPLY_HE",
+                             details:"verification_method must be chosen from COMPLY_HE")
+            )
+            return
+        }
+        
+        guard
+            let rootViewController = UIApplication.shared.windows.filter({
+                (
+                    w
+                ) -> Bool in
+                return w.isHidden == false
+            }).first?.rootViewController
+        else {
+            result(
+                FlutterError()
+            )
+            return
+        }
+        
+        let parameters = VerifyIdChipParametersBuilder(document: liquidDocumentType, verificationMethod: verificationMethod)
+            .build()
+        
+        LiquidEkyc.verifyIdChip(parameters,
+                                on: rootViewController,
+                                completion: {
+            chipResult in
+            print(chipResult)
+            result(
+                chipResult.toDictionary()
+            )
+        })
     }
-
-    if applicant != nil && token != nil {
-      LiquidEkyc.startVerify(
-        endpoint: URL(fileURLWithPath: endpoint),
-        token: token!,
-        applicant: applicant!,
-        apiKey: apiKey
-      )
-
-      result(nil)
-      return
+    
+    func verifyFace(
+        _ call: FlutterMethodCall,
+        result: @escaping FlutterResult
+    ) {
+        guard
+            let rootViewController = UIApplication.shared.windows.filter({
+                (
+                    w
+                ) -> Bool in
+                return w.isHidden == false
+            }).first?.rootViewController
+        else {
+            result(
+                FlutterError()
+            )
+            return
+        }
+        
+        let parameters = VerifyFaceParametersBuilder()
+            .setFaceVerificationType(
+                FaceVerificationType.active
+            )
+            .setShowReviewScreen(
+                true
+            )
+            .build()
+        LiquidEkyc.verifyFace(
+            parameters,
+            on: rootViewController
+        ) { verifyFaceResult in
+            result(
+                verifyFaceResult.toDictionary()
+            )
+        }
     }
-
-    let _ = LiquidEkyc.startVerify(
-      endpoint: URL(fileURLWithPath: endpoint),
-      apiKey: apiKey,
-      completion: { param in
-        result(param.toDictionary())
-      }
-    )
-  }
-
-  func identifyIdChip(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    guard
-      let rootViewController = UIApplication.shared.windows.filter({ (w) -> Bool in
-        return w.isHidden == false
-      }).first?.rootViewController
-    else {
-      result(FlutterError())
-      return
-    }
-
-    let parameters = IdentifyIdChipParametersBuilder()
-      .setVerificationMethodJpki(.complyWa)
-      .setEnabledChipForgotPin(false)
-      .setDocumentTypeJpki(.mynumberCardWithMyNumber)
-      .build()
-    LiquidEkyc.identifyIdChip(parameters, on: rootViewController, completion: { chipResult in
-      result(chipResult.toDictionary())
-    })
-  }
-  
-  func activate(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    LiquidEkyc.activate(completion: { param in
-      result(param.toDictionary())
+    
+    
+    func activate(
+        _ call: FlutterMethodCall,
+        result: @escaping FlutterResult
+    ) {
+        LiquidEkyc.activate(completion: {
+            param in
+            result(
+                param.toDictionary()
+            )
     })
   }
 }
